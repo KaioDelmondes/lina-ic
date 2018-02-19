27123c6e42dcab43c5e62e63c5b3cf529cbf1501
KassioVenicios <kassio_venicios@hotmail.com>
2018-02-19 00:23:54 -0300

    Utilizando lesscss.

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
+from lesscss import LessCSS
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
+
+LessCSS(media_dir='static', compressed=False)
 
 @app.route("/")
 def server():
