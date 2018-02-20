7e58690d94e51d47e0a909483afccf6bdea267d2
KassioVenicios <kassio_venicios@hotmail.com>
2018-02-20 12:14:09 -0300

    Servidor adaptado para executar no Windows.

--- server.py
+++ server.py
@@ -8,3 +8,7 @@ LessCSS(media_dir='static', compressed=False)
 @app.route("/")
 def server():
     return render_template('index.html')
+
+# Usado para executar no Windows
+if __name__ == "__main__":
+    app.run()
