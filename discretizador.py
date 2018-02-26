73060ac9860355ac98976c820a702181c95dfb15
KaioDelmondes <kaioregodearaujo@hotmail.com>
2018-02-26 10:57:27 -0300

    Correção do Parâmetro EWD

--- discretizador.py
+++ discretizador.py
@@ -5,12 +5,12 @@ def discretizador(bd, categorias, metodo):
 	bdd = []
 	infor = []
 	
-	for j in range(0, len(bd[1])):
+	for j in range(0, len(categorias)):
 		attb_adis = disc(bd[:,j], categorias[j])
 		if metodo is "EFD":
 			attb_dis = (attb_adis.EFD())
 		elif metodo is "EWD":
-			attb_dis = (attb_adis.EFD())
+			attb_dis = (attb_adis.EWD())
 		bdd.append(attb_dis[0])
 		infor.append(attb_dis[1])
 		
