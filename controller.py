1767cf4c4313b952fcff8469f4dafaa636e3eba5
KassioVenicios <kassio_venicios@hotmail.com>
2018-03-16 12:59:08 -0300

    Servidor adaptado para a nova forma de requisição.

--- controller.py
+++ controller.py
@@ -1,21 +1,41 @@
 import os
 from werkzeug.utils import secure_filename
 from readData import readData
-from discretizador import discretizador
+import inputs
 
-def main(data, file):
+'''
+Método responsável por obter as inputs que serão
+renderizadas no formulário da página inicial.
+'''
+def get_inputs():
+    return inputs.inputs
+
+'''
+Método responsável por salvar o arquivo no servidor.
+Parâmetro: 
+    file: arquivo advindo da request.
+Retorna o nome do arquivo.
+'''
+def save_file(file):
     filename = secure_filename(file.filename)
     file.save(os.path.join('databases', filename))
-    print(os.path.join('databases', filename))
-    rd = readData(os.path.join('databases', filename))
-    bd, frames = rd.leitorCSV()
-    discretizar(data, bd)
+    return filename
+
+'''
+Método responsável por encaminhar a execução dos algoritmos.
+Busca o arquivo salvo através do filename.
+Discretização.
+Classificação.
+Retorna.
+'''
+def main(data):
+    base = readData(os.path.join('databases', data['filename']))
+    discretization(data, base)
     classificar()
 
-def discretizar(data, bd):
-    metodo = data['alg_disc']
-    a, b = discretizador(bd, [3,3,3,3], metodo)
-    return a, b
+def discretization(data, base):
+    method = data['alg_disc']
+    bdd, infor, grupos = base.dataPreparation()#(method)
 
 def classificar():
     pass
--- server.py
+++ server.py
@@ -3,29 +3,52 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import jsonify
-from controller import main
-import inputs
+import controller
 
 app = Flask(__name__)
 
+# Configuração do Less
 LessCSS(media_dir='static', compressed=False)
 
+'''
+URL responsável por renderizar a página inicial
+Obtém os campos que serão renderizados no formulário
+Renderiza a página inicial enviando os inputs.
+'''
 @app.route("/")
 def server():
-    data = inputs.get_inputs()
+    data = controller.get_inputs()
     return render_template('index.html', inputs=data)
 
+'''
+URL responsável por realizar o upload do arquivo referente a base de dados
+Obtém o arquivo advindo da request.
+Salva o arquivo no servidor com um nome seguro.
+Retorna o nome do arquivo
+'''
+@app.route("/upload", methods=['POST'])
+def upload():
+    file = request.files['file']
+    filename = controller.save_file(file)
+    return filename
+
+'''
+URL responsável pela execução dos algoritmos
+Obtém os dados advindos do formulário.
+Envia os dados ao controller para que este cuide da execução dos algoritmos
+Retorna os dados da execução dos algoritmos.
+'''
 @app.route("/exec", methods=['POST'])
 def executar():
     data = {
+        'filename' : request.form['filename'],
         'alg_disc' : request.form['alg_disc'],
         'alg_clas' : request.form['alg_clas'],
         'faixas' : request.form['faixas'],
         'treino' : request.form['treino'],
         'varv' : request.form['varv'],
     }
-    file = request.files['database']
-    #main(data, file)
+    controller.main(data)
     return jsonify(data)
 
 
